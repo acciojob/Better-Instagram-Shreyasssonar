321daaa170982707df872c32737480643e304337
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-08-20 09:29:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".image");
+  let draggedImage = null;
+
+  images.forEach((image) => {
+    image.addEventListener("dragstart", (e) => {
+      draggedImage = e.target;
+    });
+
+    image.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    image.addEventListener("drop", (e) => {
+      e.preventDefault();
+      if (draggedImage !== null && draggedImage !== image) {
+        // Swap content
+        const tempContent = image.innerHTML;
+        image.innerHTML = draggedImage.innerHTML;
+        draggedImage.innerHTML = tempContent;
+      }
+      draggedImage = null;
+    });
+  });
+});
